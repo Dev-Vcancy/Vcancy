635e57d1b4b36424fb8e4045d6d695a772ee1fb5
Abhishek Arora <abhishekarora1028@gmail.com>
2017-10-11 15:42:01 +0530

    validation for valid email and working of exernal applications

--- login/app/scripts/controllers/tenantapp.js
+++ login/app/scripts/controllers/tenantapp.js
@@ -10,12 +10,13 @@ vcancyApp
 		var vm = this;
 		var tenantID = localStorage.getItem('userID');
 		vm.loader = 1;
+		vm.submittedappsavail = 0;
+		vm.submitappsdata = [];		
 		
 		firebase.database().ref('applyprop/').orderByChild("tenantID").equalTo(tenantID).once("value", function(snapshot) {	
-			console.log(snapshot.val())
+			// console.log(snapshot.val())
 			$scope.$apply(function(){
-				if(snapshot.val() != null) {
-					
+				if(snapshot.val() != null) {					
 					vm.pendingappsavail = 0;
 					
 					//to map the object to array
@@ -24,12 +25,9 @@ vcancyApp
 							vm.pendingappsavail = 1;
 							return [{scheduleID:index, address:value.address, dateslot: value.dateslot, timerange: value.timerange,  schedulestatus: value.schedulestatus}];
 						} 
-						// if(value.schedulestatus == "confirmed" && moment(value.dateslot).isSame(new Date())  &&   moment(value.fromtimeslot).format("HH:mm") < moment(new Date()).format("HH:mm") &&  moment(value.to).format("HH:mm") < moment(new Date()).format("HH:mm")) {
-							// return [{scheduleID:index, address:value.address, dateslot: value.dateslot, timerange: value.timerange,  schedulestatus: value.schedulestatus}];
-						// }
 					});	
 					
-					console.log(vm.tabledata);
+					// console.log(vm.tabledata);
 					vm.extracols = [
 						  { field: "scheduleID", title: "", show: true }
 						];
@@ -66,35 +64,33 @@ vcancyApp
 					 // dataset: vm.tabledata
 				})
 				
-				vm.submittedappsavail = 0;
-				vm.submitappsdata = [];				
-				$.map(snapshot.val(), function(val, key) {					
-					firebase.database().ref('submitapps/').orderByChild("scheduleID").equalTo(key).once("value", function(snap) {	
-						// console.log(snap.val())
-						$scope.$apply(function(){
-							if(snap.val() !== null) {					
-								//to map the object to array
-								$.map(snap.val(), function(value, index) {	
-									if(val.schedulestatus == "submitted" ){
-										vm.submittedappsavail = 1;
-										vm.submitappsdata.push({appID:index, address:value.address, dated: value.dated, rentalstatus: value.rentalstatus});
-									}
-									
-									
-								});	
-							}
+				
+				
+				if(snapshot.val() != null){
+					$.map(snapshot.val(), function(val, key) {		
+						firebase.database().ref('submitapps/').orderByChild("scheduleID").equalTo(key).once("value", function(snap) {	
+							$scope.$apply(function() {
+								if(snap.val() !== null) {					
+									//to map the object to array
+									$.map(snap.val(), function(value, index) {	
+										if(val.schedulestatus == "submitted" ){
+											vm.submittedappsavail = 1;
+											vm.submitappsdata.push({appID:index, address:value.address, dated: value.dated, rentalstatus: value.rentalstatus});
+										}
+										
+										
+									});	
+								}
+							});
 						});
-					});
+					});	
 					
 					vm.submitappscols = [
-						  { field: "address", title: "Address", sortable: "address", show: true },
-						  { field: "dated", title: "Submitted On", sortable: "dated", show: true },
-						  { field: "rentalstatus", title: "Status", sortable: "rentalstatus", show: true }
-						];
-						
+					  { field: "address", title: "Address", sortable: "address", show: true },
+					  { field: "dated", title: "Submitted On", sortable: "dated", show: true },
+					  { field: "rentalstatus", title: "Status", sortable: "rentalstatus", show: true }
+					];
 					
-					vm.loader = 0;	
-						
 					//Sorting
 					vm.submitappsSorting = new NgTableParams({
 						sorting: {address: 'asc'}}, 
@@ -110,37 +106,67 @@ vcancyApp
 							$defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
 						}*/
 						 // dataset: vm.submitappsdata
-					})
-				});
+					})	
+				}
 			});		
+		});	
+		
+		
+		firebase.database().ref('submitapps/').orderByChild("tenantID").equalTo(tenantID).once("value", function(snapshot) {	
+			$scope.$apply(function(){
+				if(snapshot.val() != null){
+					$.map(snapshot.val(), function(value, key) {		
+						if(value.scheduleID == 0){
+							vm.submittedappsavail = 1;
+							vm.submitappsdata.push({appID:key, address:value.address, dated: value.dated, rentalstatus: value.rentalstatus});
+						}
+					});
+					
+					vm.submitappscols = [
+					  { field: "address", title: "Address", sortable: "address", show: true },
+					  { field: "dated", title: "Submitted On", sortable: "dated", show: true },
+					  { field: "rentalstatus", title: "Status", sortable: "rentalstatus", show: true }
+					];
+
+					//Sorting
+					vm.submitappsSorting = new NgTableParams({
+						sorting: {address: 'asc'}}, 
+					
+					{dataset: vm.submitappsdata
+					/*}, {
+						total: vm.submitappsdata.length, // length of data
+						getData: function($defer, params) {
+							// console.log(params);
+							// use build-in angular filter
+							var orderedData = params.sorting() ? $filter('orderBy')(vm.submitappsdata, params.orderBy()) : vm.submitappsdata;
+				
+							$defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+						}*/
+						 // dataset: vm.submitappsdata
+					})	
+				}			
+					
+			});
 		});
-		// firebase.database().ref('submitapps/').orderByChild("scheduleID").equalTo('0').once("value", function(snap) {	
-			// console.log(snap.val())
-			// $scope.$apply(function(){
-				// if(snap.val() !== null) {		
-					// $.map(snap.val(), function(value, index) {	
-						// vm.submittedappsavail = 1;
-						// vm.submitappsdata.push({appID:index, address:value.address, dated: value.dated, rentalstatus: value.rentalstatus});
-					// });
-				// }
-			// });
-		// });
-				
-				
+		
 		if(vm.submittedappsavail == 0) {
 			vm.submitappsdata.push({scheduleID:'', name:'', age: '', profession: '',salary: '', pets: '', maritalstatus:'', appno:'',  schedulestatus: ''});
+		} else {			
+			vm.loader = 0;				
 		}
-		
-		
+				
+				
 		vm.email = '';
 		vm.disablebutton = 1;
 		vm.emailrequired = function(event){
-			if(vm.email == ''){
+			if(vm.email == '' || !vm.email.match(/^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/)){
 				vm.disablebutton = 1;
 			} else {
 				vm.disablebutton = 0;
 			}
 		}
+		
+		
 		vm.gotoRental = function(event){
 			if(vm.disablebutton == 0){
 				window.location.href = "#/rentalform/0";
