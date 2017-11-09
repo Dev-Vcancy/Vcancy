1f1753b1b37ee36e9aa4e726c6792bb84d030de6
Faizan <developers.vcancy@gmail.com>
2017-11-08 19:04:07 -0800

    emailtemplate updated

--- devVcancy/app/scripts/controllers/schedule.js
+++ devVcancy/app/scripts/controllers/schedule.js
@@ -121,7 +121,7 @@ vcancyApp
 				})
 				firebase.database().ref('applyprop/'+index).once("value", function(snapshot) {
 					firebase.database().ref('users/'+snapshot.val().tenantID).once("value", function(snap) {
-						var emailData = '<p>Hello '+snapshot.val().name+', </p><p>Your viewing time '+snapshot.val().dateslot+', '+snapshot.val().timerange+' has been been <strong>cancelled</strong> by the landlord of '+snapshot.val().address+'.</p><p>Please book another time using the link initially provided or contact the landlord directly.</p><p>To view details, please log in  http://www.vcancy.ca/login/#/ and go to “Schedule”</p><p>If you have any questions or suggestions please email us at support@vcancy.ca</p><p>Thanks,</p><p>Team Vcancy</p>';
+						var emailData = '<p>Hello '+snapshot.val().name+', </p><p>Your viewing time '+snapshot.val().dateslot+', '+snapshot.val().timerange+' has been been <strong>cancelled</strong> by the landlord of '+snapshot.val().address+'.</p><p>Please book another time using the link initially provided or contact the landlord directly.</p><p>To view details, please <a href="http://www.vcancy.ca/login/#/"> log in </a>  and go to “Schedule”</p><p>If you have any questions or suggestions please email us at support@vcancy.ca</p><p>Thanks,</p><p>Team Vcancy</p>';
 						
 						emailSendingService.sendEmailViaNodeMailer(snap.val().email, 'Your viewing has been cancelled for '+snapshot.val().address, 'cancelstatus', emailData);
 					});
