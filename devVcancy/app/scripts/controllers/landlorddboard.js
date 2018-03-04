970ed305ef2cd8919fc13a2efab4ef7eafcd505f
ganipadela <ganipadela@gmail.com>
2018-03-04 13:45:56 +0530

    Landlord dashboard issue fixed

--- devVcancy/app/scripts/controllers/landlorddboard.js
+++ devVcancy/app/scripts/controllers/landlorddboard.js
@@ -10,7 +10,7 @@ vcancyApp
 		var vm = this;
 		var landlordID = localStorage.getItem('userID');
 		vm.proplive = 0;
-		vm.units = 0;
+		vm.unitsCount = 0;
 		vm.vacantUnits = 0;
 		vm.appliedProperties;
 		vm.viewingschedule = 0;
@@ -264,11 +264,11 @@ vcancyApp
 			var properties = snapshot.val() || {};
 			$scope.$apply(function () {
 				var liveProperties = 0;
-				var units = 0;
+				var unitsCount = 0;
 				var vacantUnits = 0;
 				_.forEach(properties, function (value, key) {
-					if(value.units == "multiple" && value.unitlists) {
-						units = units + value.unitlists.length;
+					if(value.unitlists) {
+						unitsCount = unitsCount + value.unitlists.length;
 						var _vacantUnits = _.sumBy(value.unitlists, function(unitObj) {
 							if(unitObj.status == "" || unitObj.status == "available" || !unitObj.status) {
 								return 1;
@@ -280,7 +280,7 @@ vcancyApp
 					liveProperties = liveProperties + 1;
 				});
 				vm.proplive = liveProperties;
-				vm.units = units;
+				vm.unitsCount = unitsCount;
 				vm.vacantUnits = vacantUnits;
 				vm.loader = 0;
 			});
@@ -291,26 +291,28 @@ vcancyApp
 			console.log(snapshot.val())
 			var appliedProperties = 0;
 			$scope.$apply(function () {
-				$.map(snapshot.val(), function (value, index) {
-
-					if (value.schedulestatus == "confirmed" && moment(value.dateslot).isBefore(new Date())) {
-						vm.viewed += 1;
-					}
-					if (value.schedulestatus == "confirmed" && moment(value.dateslot).isAfter(new Date())) {
-						vm.viewingschedule += 1;
-					}
-					if (value.schedulestatus == "confirmed" && moment(value.dateslot).isSame(new Date()) && moment(value.fromtimeslot).format('HH:mm') < moment(new Date()).format('HH:mm') && moment(value.toslot).format('HH:mm') < moment(new Date()).format('HH:mm')) {
-						vm.viewed += 1;
-					}
-					if (value.schedulestatus == "confirmed" && moment(value.dateslot).isSame(new Date()) && moment(value.fromtimeslot).format('HH:mm') >= moment(new Date()).format('HH:mm') && moment(value.toslot).format('HH:mm') >= moment(new Date()).format('HH:mm')) {
-						vm.viewingschedule += 1;
-					}
-					if (value.schedulestatus == "submitted") {
-						vm.submitapps += 1;
-					}
-
-					appliedProperties = appliedProperties + 1;
-				});
+				if(snapshot.val()) {
+					$.map(snapshot.val(), function (value, index) {
+	
+						if (value.schedulestatus == "confirmed" && moment(value.dateslot).isBefore(new Date())) {
+							vm.viewed += 1;
+						}
+						if (value.schedulestatus == "confirmed" && moment(value.dateslot).isAfter(new Date())) {
+							vm.viewingschedule += 1;
+						}
+						if (value.schedulestatus == "confirmed" && moment(value.dateslot).isSame(new Date()) && moment(value.fromtimeslot).format('HH:mm') < moment(new Date()).format('HH:mm') && moment(value.toslot).format('HH:mm') < moment(new Date()).format('HH:mm')) {
+							vm.viewed += 1;
+						}
+						if (value.schedulestatus == "confirmed" && moment(value.dateslot).isSame(new Date()) && moment(value.fromtimeslot).format('HH:mm') >= moment(new Date()).format('HH:mm') && moment(value.toslot).format('HH:mm') >= moment(new Date()).format('HH:mm')) {
+							vm.viewingschedule += 1;
+						}
+						if (value.schedulestatus == "submitted") {
+							vm.submitapps += 1;
+						}
+	
+						appliedProperties = appliedProperties + 1;
+					});
+				}
 				vm.appliedProperties = appliedProperties;
 				vm.loader = 0;
 			});
