e2867cbdc5c0ef5d3dc1f05f1fd1e899006610b7
pvakharia9033 <pvakharia007@gmail.com>
2018-03-04 09:42:44 +0530

    images added

--- devVcancy/app/scripts/controllers/tenantapplyprop.js
+++ devVcancy/app/scripts/controllers/tenantapplyprop.js
@@ -129,7 +129,7 @@ vcancyApp.controller('applypropCtrl', ['$scope', '$firebaseAuth', '$state', '$ro
 				if (snap.val()) {
 					var propData = snap.val();
 					vm.propData = angular.copy(propData);
-					console.log(propData)
+					console.log('propData', propData)
 					if (propData && propData.unitlists && propData.unitlists.length > 0) {
 						vm.selectedUnit = propData.unitlists.find(function (unit) {
 							if (unit.unit == vm.unitId) return true;
