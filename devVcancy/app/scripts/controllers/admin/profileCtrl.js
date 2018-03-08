439bceaca5dca7fdaa0721d78dfc5a092e77e538
pvakharia9033 <pvakharia007@gmail.com>
2018-03-08 18:51:39 +0530

    admin pannel added

---
+++ devVcancy/app/scripts/controllers/admin/profileCtrl.js
@@ -0,0 +1,11 @@
+'use strict';
+
+vcancyApp
+    .controller('adminProfileCtrl', ['$scope', '$firebaseAuth', '$state', '$rootScope', '$stateParams', '$window', '_',
+        function ($scope, $firebaseAuth, $state, $rootScope, $stateParams, $window, _) {
+
+            var vm = this;
+            var landlordID = localStorage.getItem('userID');
+            vm.userData = localStorage.getItem('userData') ? JSON.parse(localStorage.getItem('userData')) : null;
+
+        }]);
