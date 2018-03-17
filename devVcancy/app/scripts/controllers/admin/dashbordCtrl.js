e1050160ea3c0212068daee8567a091eff2a7c38
pvakharia9033 <pvakharia007@gmail.com>
2018-03-17 19:31:28 +0530

    dashboard completed

--- devVcancy/app/scripts/controllers/admin/dashbordCtrl.js
+++ devVcancy/app/scripts/controllers/admin/dashbordCtrl.js
@@ -5,7 +5,61 @@ vcancyApp
         function ($scope, $firebaseAuth, $state, $rootScope, $stateParams, $window, _) {
 
             var vm = this;
-            var landlordID = localStorage.getItem('userID');
-            vm.userData = localStorage.getItem('userData') ? JSON.parse(localStorage.getItem('userData')) : null;
+            vm.totalUsers = 0;
+            vm.totalProperty = 0;
+            vm.totalUnit = 0;
+            vm.totalVacant = 0;
+            vm.totalApplication = 0;
+            vm.totalShowingScheduled = 0;
+
+            firebase.database().ref('users/').once("value", function (snapvalue) {
+                var users = snapvalue.val();
+                $scope.$apply(function () {
+                    vm.totalUsers = _.size(users);
+                });
+            });
+
+            firebase.database().ref('properties/').once("value", function (snapvalue) {
+                var properties = snapvalue.val();
+                $scope.$apply(function () {
+                    vm.totalProperty = _.size(properties);
+
+                    vm.totalUnit = _.reduce(properties, function (previousValue, currentValue, currentIndex) {
+                        previousValue += _.size(currentValue.unitlists);
+                        return previousValue;
+                    }, 0);
+
+                    vm.totalVacant = _.reduce(properties, function (previousValue, currentValue, currentIndex) {
+                        previousValue += _.reduce(currentValue.unitlists, function (pv, cv, ci) {
+                            if (cv.status === 'available' || cv.status === 'availablesoon') {
+                                pv += 1;
+                            }
+                            return pv;
+                        }, 0);
+                        return previousValue;
+                    }, 0);
+                });
+
+            });
+
+            firebase.database().ref('submitapps/').once("value", function (snapvalue) {
+                var application = snapvalue.val();
+                $scope.$apply(function () {
+                    vm.totalApplication = _.size(application);
+                });
+            });
+
+            firebase.database().ref('applyprop/').once("value", function (snapvalue) {
+                var scheduled = snapvalue.val();
+                console.log(scheduled)
+                $scope.$apply(function () {
+                    vm.totalShowingScheduled = _.reduce(scheduled, function (pv, cv, ci) {
+                        if (cv.schedulestatus == "scheduled" || cv.schedulestatus == "submitted") {
+                            pv += 1;
+                        }
+                        return pv;
+                    }, 0);
+                });
+            });
 
         }]);
